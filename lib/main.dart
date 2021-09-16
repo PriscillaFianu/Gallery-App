7e69c1b2094a073eac86019e20cc2003a598caa7
priscilla Fianu <priscillafianu80@gmail.com>
2021-09-16 13:36:44 +0000

    added another card

--- lib/main.dart
+++ lib/main.dart
@@ -166,6 +166,112 @@ class Homepage extends StatelessWidget {
 
                 ),
                 ),
+               Card(
+                color: Colors.white,
+                margin: EdgeInsets.fromLTRB(16, 10, 16, 0),
+                child: Column(children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'),
+                    ),
+                    title: Text(
+                      'Naa Dede Adepa',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
+                    ),
+                    subtitle: Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          size: 14,
+                          color: Colors.blue,
+                        ),
+                        Text('Bahamas, United States'),
+                      ],
+                    ),
+                  ),
+                  Image.network(
+                      'https://images.unsplash.com/photo-1511690656952-34342bb7c2f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
+                      height: 218,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Material(
+                            color: Colors.grey.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(35),
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.favorite,
+                                    color: Colors.red,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    '560',
+                                    style: TextStyle(fontSize: 14),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          FlutterImageStack(
+                            imageList: _images,
+                            showTotalCount: false,
+                            totalCount: 4,
+                            itemRadius: 35, // Radius of each images
+                            itemCount:
+                                4, // Maximum number of images to be shown in stack
+                            itemBorderWidth:
+                                2, // Border width around the images
+                          ),
+                        ],
+                      ),
+                      Material(color: Colors.grey.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(35),
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    CupertinoIcons.chat_bubble_fill,
+                                    color: Colors.grey,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    '700',
+                                    style: TextStyle(fontSize: 14),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+
+                    ],
+                    
+
+                  ),
+                ],
+
+                ),
+                ),
+
           ],
         )));
   }
